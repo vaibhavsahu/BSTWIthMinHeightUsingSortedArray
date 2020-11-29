200c1d7a9488104780316fb07e476c55514b8a03
Vaibhav Sahu <vsahu@overstock.com>
2020-11-29 12:16:48 -0700

    CTC Chap04 Q02 - Minimal BST using sorted array

---
+++ src/com/vaibhav/example/Main.java
@@ -0,0 +1,37 @@
+package com.vaibhav.example;
+
+public class Main {
+
+    private static int index = 0;
+
+    public static void main(String[] args) {
+	    int [] sortedArray = {1,2,7,8,11,13,15};
+	    Node root = makeBSTArray(sortedArray, 0, sortedArray.length-1, null);
+	    printTree(root);
+    }
+
+    private static void printTree(Node root) {
+        if(root == null){
+            return;
+        }
+        System.out.println(root.getData());
+        printTree(root.getLeftChild());
+        printTree(root.getRightChild());
+    }
+
+    private static Node makeBSTArray(int[] sortedArray, int start, int end, Node root1) {
+        //base case: if start and end are same, return
+            if(start == end){ //leaf node
+                Node node = new Node(sortedArray[start]);
+                return node;
+            }
+            int middle = (start+end)/2;
+            root1 = new Node(sortedArray[middle]);
+
+            root1.setLeftChild(makeBSTArray(sortedArray, start, middle-1, root1.getLeftChild()));
+
+            root1.setRightChild(makeBSTArray(sortedArray, middle+1, end, root1.getRightChild()));
+
+        return root1;
+    }
+}
---
+++ src/com/vaibhav/example/Node.java
@@ -0,0 +1,45 @@
+package com.vaibhav.example;
+
+public class Node {
+    //public Node left;
+    private int data;
+    private Node leftChild;
+    private Node rightChild;
+
+    public Node(int data) {
+        this.data = data;
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
+}
